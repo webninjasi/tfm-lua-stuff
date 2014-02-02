b2919b02cf669ba760eb88e949e228249d91e27e
WebNinjasi <webninjasi.0@gmail.com>
2014-02-02 13:20:24 +0200

    add error blocker

---
+++ errblock.lua
@@ -0,0 +1,28 @@
+-- Error Informer
+--[[
+This prevents the script from stopping when a runtime error occurred and informs you about the error
+]]
+
+function sendDevMsg(m)
+	local devs={'Transforlays'}
+	for i,n in pairs(devs) then
+		if tfm.get.room.playerList[n] then
+			tfm.exec.chatMessage('<R>[ERR] <BL>'..m:gsub('<','&lt;'),n)
+		end
+	end
+end
+do
+	local f={}
+	local function c(k)
+		return function(...)
+			local s,m=pcall(f[k],...)
+			if not s then sendDevMsg(m) end
+		end
+	end
+	for k,v in pairs(_G) do
+		if type(v)=='function' and type(k)=='string' and k:sub(1,5)=='event' then
+			f[k]=v
+			_G[k]=c(k)
+		end
+	end
+end
