78298ed442f0e81f95700995368f96db9bcdf355
webninjasi <webninjasi.0@gmail.com>
2014-03-03 13:19:33 +0200

    Add updateTextArea to warnblock

--- warnblock.lua
+++ warnblock.lua
@@ -9,6 +9,14 @@ do
 		end
 	end
 
+	local updateTextArea = ui.updateTextArea
+	ui.updateTextArea = function(ID, text, ...)
+		if ID and text then
+			updateTextArea(ID, tostring(text):sub(1, 2e3), ...)
+			return true
+		end
+	end
+
 	local addPopup = ui.addPopup
 	ui.addPopup = function(ID, type, text, ...)
 		if ID and type and text then
@@ -31,14 +39,22 @@ end
 assert(not ui.addTextArea())
 assert(not ui.addTextArea(1))
 assert(not ui.addTextArea(nil, "Test1"))
-assert(ui.addTextArea(2, string.rep('.', 2e3) .. "Test2"))
-assert(ui.addTextArea(3, "Test3"))
+assert(ui.addTextArea(2, string.rep('.', 2e3) .. "Test2", nil, 50, 50))
+assert(ui.addTextArea(3, "Test3", nil, 50, 100))
+
+assert(not ui.updateTextArea())
+assert(not ui.updateTextArea(1))
+assert(not ui.updateTextArea(nil, "Test4"))
+assert(ui.updateTextArea(2, string.rep('.', 2e3) .. "Test5"))
+assert(ui.updateTextArea(3, "Test6"))
+
 assert(not ui.addPopup())
 assert(not ui.addPopup(1))
 assert(not ui.addPopup(1, 2))
-assert(not ui.addPopup(1, nil, "Test4"))
-assert(not ui.addPopup(nil, 2, "Test5"))
-assert(ui.addPopup(1, 2, "Test5"))
+assert(not ui.addPopup(1, nil, "Test7"))
+assert(not ui.addPopup(nil, 2, "Test8"))
+assert(ui.addPopup(1, 2, "Test9"))
+
 assert(tfm.exec.newGame(5))
 assert(not tfm.exec.newGame(4))
 --]]
